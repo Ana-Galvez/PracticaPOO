54409cf555eb2d49f14fd4949b51ae193dcb4ca1
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2025-01-04 23:51:26 -0300

    feat: pi and pow

--- PracticaPOO/Program.cs
+++ PracticaPOO/Program.cs
@@ -10,26 +10,25 @@ namespace PracticaPOO
             Circle circle1 = new Circle();
             Circle circle2 = new Circle();
             Circle circle3 = new Circle();
-            Console.WriteLine($"El área del círculo es: {circle1.calcularArea(5)}");
-            Console.WriteLine($"El área del círculo es: {circle2.calcularArea(10)}");
-            Console.WriteLine($"El área del círculo es: {circle3.calcularArea(8)}");
-
+            Console.WriteLine($"El área del círculo es: {circle1.CalcularArea(5)}");
+            Console.WriteLine($"El área del círculo es: {circle2.CalcularArea(10)}");
+            Console.WriteLine($"El área del círculo es: {circle3.CalcularArea(8)}");
         }
     }
 
     class Circle
     {
         // propiedad o campo de clase. MUY MALA PRÁCTICA SI SE QUIERE CAMBIAR EN UN OBJETO.DEBE SER PRIVADO. POR DEFECTO ES PRIVADO
-        const double PI = 3.1416;  
+        const double PI = Math.PI;  
 
         /// <summary>
         /// Método para calcular el área de un círculo
         /// </summary>
-        /// <param name="radio">Se pide el radio como entero o con decimal</param>
+        /// <param name="radio"></param>
         /// <returns>Devuelve el área del círculo</returns>
-        public double calcularArea(double radio)
+        public double CalcularArea(double radio)
         {
-            return PI * radio * radio;
+            return PI * Math.Pow(radio,2);
         }
     }
 }
